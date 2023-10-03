609369e4b808628bee33a4072d05af326c9c68dd
tayamirai9 <128257364+tayamirai9@users.noreply.github.com>
2023-10-03 11:16:33 +0100

    - Finished galleryView by adding GeometryReader, ScrollView, linking it to DataService

--- Restaurant/GalleryView.swift
+++ Restaurant/GalleryView.swift
@@ -2,8 +2,41 @@
 import SwiftUI
 
 struct GalleryView: View {
+    
+    @State var photos = [String]()
+    var dataService = DataService()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading){
+            
+            Text("Gallery")
+                .font(.largeTitle)
+                .bold()
+            
+            GeometryReader{ proxy in
+                ScrollView (showsIndicators: false){
+                    LazyVGrid(columns: [
+                        GridItem(spacing: 10),
+                        GridItem(spacing: 10),
+                        GridItem(spacing: 10)],
+                              spacing: 10){
+                        ForEach(photos, id: \.self){ p in
+                            Image(p)
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: (proxy.size.width - 20) / 3)
+                                .clipped()
+                        }
+                    }
+                }
+
+            }
+            
+        }
+        .padding(.horizontal)
+        .onAppear{
+            photos = dataService.getPhotos()
+        }
     }
 }
 
